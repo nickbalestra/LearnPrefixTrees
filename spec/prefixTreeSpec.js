1f10b6e2606240b8a38493fdd93c86cd9f74b8d2
Nick Balestra <nick@balestra.ch>
2015-10-11 17:22:50 -0700

    Fixed t9 hash and included in the src file

--- spec/prefixTreeSpec.js
+++ spec/prefixTreeSpec.js
@@ -8,12 +8,11 @@ describe('PrefixTree', function() {
   it('should have properties named "children", "words" and "keys"', function() {
     expect(prefixTree.hasOwnProperty("children")).to.equal(true);
     expect(prefixTree.hasOwnProperty("words")).to.equal(true);
-    expect(prefixTree.hasOwnProperty("keys")).to.equal(true);
   });
 
   it('it should handle numerical to letter conversion of phone inputs (T9-style texting)', function() {
-    expect(prefixTree.keys['a']).to.equal(1);
-    expect(prefixTree.keys['z']).to.equal(8);
+    expect(keys['a']).to.equal(2);
+    expect(keys['z']).to.equal(9);
   });
 
   it('should have methods named "insert" and "getSuggestions"', function() {
@@ -24,14 +23,15 @@ describe('PrefixTree', function() {
 
   it('it should add "hello" to the tree', function() {
     var returned = prefixTree.insert("hello");
-    expect(prefixTree.getSuggestions("32445").indexOf("hello") >= 0).to.equal(true);
+    expect(prefixTree.getSuggestions("43556").indexOf("hello") >= 0).to.equal(true);
   });
 
   it('it should add multiple words to the tree', function() {
     prefixTree.insert("hello");
     prefixTree.insert("hellooo");
     prefixTree.insert("hellloo");
-    var suggestions  = prefixTree.getSuggestions("3244", 3);
+    var suggestions  = prefixTree.getSuggestions("4355", 3);
+    console.log(suggestions);
     expect(suggestions.indexOf("hello") >= 0).to.equal(true);
     expect(suggestions.indexOf("hellloo") >= 0).to.equal(true);
     expect(suggestions.indexOf("hellooo") >= 0).to.equal(true);
@@ -41,30 +41,29 @@ describe('PrefixTree', function() {
     prefixTree.insert("hello");
     prefixTree.insert("hellooo");
     prefixTree.insert("hellloo");
-    var suggestions  = prefixTree.getSuggestions("3244", 4);
+    var suggestions  = prefixTree.getSuggestions("4355", 4);
     expect(suggestions.length).to.equal(3);
   });
 
-  // (Extra credit! Remove the extra "x" when you want the following tests to run)
-  xit('it should be case insensitive', function() {
+  it('it should be case insensitive', function() {
     var returned = prefixTree.insert("hELlO");
-    expect(prefixTree.getSuggestions("32445").indexOf("hello") >= 0).to.equal(true);
+    expect(prefixTree.getSuggestions("43556").indexOf("hello") >= 0).to.equal(true);
   });
 
 
-  xit('it should be able to suggest only worlds with max 2 characters longer', function() {
+  it('it should be able to suggest only worlds with max 2 characters longer', function() {
     prefixTree.insert("hello");
     prefixTree.insert("helloo");
     prefixTree.insert("hellooo");
-    var suggestions  = prefixTree.getSuggestions("3244", 2);
+    var suggestions  = prefixTree.getSuggestions("4355", 2);
     expect(suggestions.length).to.equal(2);
   });
 
-  xit('it should be able to suggest only worlds with max 3 characters longer', function() {
+  it('it should be able to suggest only worlds with max 3 characters longer', function() {
     prefixTree.insert("hello");
     prefixTree.insert("helloo");
     prefixTree.insert("hellooo");
-    var suggestions  = prefixTree.getSuggestions("3244", 3);
+    var suggestions  = prefixTree.getSuggestions("4355", 3);
     expect(suggestions.length).to.equal(3);
   });
 
--- src/prefixTree.js
+++ src/prefixTree.js
@@ -1,7 +1,29 @@
 // The PrefixTree that can handle autocomplete for T9-style texting
+var keys = {
+  'a': 2, 'b': 2, 'c': 2,
+  'd': 3, 'e': 3, 'f': 3,
+  'g': 4, 'h': 4, 'i': 4,
+  'j': 5, 'k': 5, 'l': 5,
+  'm': 6, 'n': 6, 'o': 6,
+  'p': 7, 'q': 7, 'r': 7, 's': 7,
+  't': 8, 'u': 8, 'v': 8,
+  'w': 9, 'x': 9, 'y': 9, 'z': 9
+};
 
 var PrefixTree = function() {
-  // Your properties go here!
+
 };
 
 // Your methods go here! Good luck!
+
+// Traverse the tree to the node where the word should be inserted. If any
+// needed nodes do not exist along the way, they are created.
+PrefixTree.prototype.insert = function(word) {
+
+};
+
+// Traverse the tree based on the key digits in keyString, to find the node
+// where relevant words are stored.
+PrefixTree.prototype.getSuggestions = function(keyString, suggestionDepth) {
+
+};
